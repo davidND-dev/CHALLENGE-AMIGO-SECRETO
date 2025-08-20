2d2775c17623bb8697eed39c75be6ee2b08e5443
David Najera <davidronaldonajeradavila@gmail.com>
2025-08-20 14:38:49 -0500

    Avance Final  del proyecto

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Crear un array para almacenar los nombres de los amigos
+let amigos = [];
+
+// Implementa una función para agregar amigos
+/* Capturar el valor del campo de entrada: Utilizar document.getElementById o document.querySelector para obtener el texto ingresado por el usuario.
+
+Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío. Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."
+
+Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombre de amigos usando el método.push().
+
+Limpiar el campo de entrada: Después de añadir el nombre, restablecer el campo de texto a una cadena vacía. */
+function agregarAmigo() {
+  let input = document.getElementById("amigo");
+  let nombre = input.value.trim();
+
+  if (nombre === "") {
+    alert("Por favor, inserte un nombre.");
+    return;
+  }
+
+  amigos.push(nombre);
+
+  input.value = "";
+
+  mostrarLista();
+}
+
+function mostrarLista() {
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = ""; // limpiar antes de mostrar de nuevo
+
+  // Recorrer el array y crear elementos <li>
+  for (let i = 0; i < amigos.length; i++) {
+    let li = document.createElement("li");
+    li.textContent = amigos[i];
+    lista.appendChild(li);
+  }
+}
+
+// Función para sortear un amigo
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("No hay amigos para sortear.");
+    return;
+  }
+
+  let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+  let amigoSorteado = amigos[indiceAleatorio];
+
+  let resultado = document.getElementById("resultado");
+  resultado.innerHTML = `<li>🎉 El amigo secreto es: <strong>${amigoSorteado}</strong></li>`;
+}
